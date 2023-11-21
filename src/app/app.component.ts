a99dbdc98c621c50f8518dd62594e2febff75ee6
Arpad Kozma <arpadkozma89@gmail.com>
2023-11-21 12:09:40 +0100

    fix loader position

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { HelperService } from "src/app/services/helper.service";
 import { Component } from "@angular/core";
-import { debounceTime } from "rxjs";
+import { debounceTime, of } from "rxjs";
 
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
 })
 export class AppComponent {
-    loading = this.helperService.loading$.pipe(debounceTime(250));
+    loading = of(true) || this.helperService.loading$.pipe(debounceTime(250));
 
     constructor(private helperService: HelperService) {}
 }
